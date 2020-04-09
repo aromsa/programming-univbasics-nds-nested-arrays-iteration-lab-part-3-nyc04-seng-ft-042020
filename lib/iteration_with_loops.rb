e042ebec0bb3c3f683adb4b517176c743c995a7b
anna bateman <annacbateman@gmail.com>
2020-04-09 20:09:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ string = []
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    if src[element_index][row_index] = ""
+    if src[element_index][row_index] == ""
       string << src[element_index][row_index]
   end
  element_index += 1
